8dd0f9ce522ecfa4e884e82807a9996dad9fc117
thurakhant <thurakhantthein4425@gmail.com>
2021-09-02 02:13:33 +0630

    final service

--- lib/AppUiWidget/service.dart
+++ lib/AppUiWidget/service.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class Service extends StatelessWidget {
@@ -6,13 +8,118 @@ class Service extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          width: 340,
-          height: 165,
-          decoration: BoxDecoration(
-            color: Colors.red,
-            borderRadius: BorderRadius.circular(10),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/Background.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Container(
+            padding: EdgeInsets.fromLTRB(11, 13, 10, 13),
+            width: 340,
+            height: 165,
+            decoration: BoxDecoration(
+              color: Colors.grey.shade200.withOpacity(0.5),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Align(
+                      alignment: Alignment.topCenter,
+                      child: Container(
+                        padding: EdgeInsets.only(left: 14),
+                        width: 319,
+                        height: 47,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: Colors.white.withOpacity(0.5),
+                        ),
+                        child: Row(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(right: 12),
+                              child: Icon(Icons.location_city),
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Your location'),
+                                Text('42 East 20th Street, NY'),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Divider(
+                  color: Colors.black,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      width: 75,
+                      height: 68,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: Colors.white.withOpacity(0.5),
+                        image: DecorationImage(
+                          image: AssetImage('images/park.png'),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 75,
+                      height: 68,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: Colors.white.withOpacity(0.5),
+                        image: DecorationImage(
+                          image: AssetImage('images/eat.png'),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(14),
+                      width: 75,
+                      height: 68,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: Colors.white.withOpacity(1),
+                        // image: DecorationImage(
+                        //   image: AssetImage('images/sleep.png'),
+                        // ),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Image.asset('images/sleep.png'),
+                          Text('Hotel'),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: 75,
+                      height: 68,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: Colors.white.withOpacity(0.5),
+                        image: DecorationImage(
+                          image: AssetImage('images/coffee.png'),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
